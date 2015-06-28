903c0ec3bf404030cc5534117e0d82f8eb62ea9c
Ninals-GitHub <mail@softwaretechnique.jp>
2015-06-28 11:02:24 +0900

    Step 4. You can simply mount.

---
+++ 004_mount_ok/me2fs.h
@@ -0,0 +1,154 @@
+/*********************************************************************************
+	File			: me2fs.h
+	Description		: Defines for my ext2 file system
+
+*********************************************************************************/
+#ifndef	__ME2FS_H__
+#define	__ME2FS_H__
+
+#include <uapi/linux/magic.h>
+
+/*
+==================================================================================
+
+	Prototype Statement
+
+==================================================================================
+*/
+
+/*
+==================================================================================
+
+	DEFINES
+
+==================================================================================
+*/
+#define	ME2FS_SUPER_MAGIC		EXT2_SUPER_MAGIC		/* 0xEF53				*/
+
+/*
+----------------------------------------------------------------------------------
+	Special Inode Number
+----------------------------------------------------------------------------------
+*/
+#define	ME2FS_EXT2_BAD_INO		1			/* bad blocks inode					*/
+#define	ME2FS_EXT2_ROOT_INO		2
+#define	ME2FS_EXT2_BL_INO		5			/* boot loader inode				*/
+#define	ME2FS_EXT2_UNDEL_DIR_NO	6			/* undelete directory inode			*/
+
+/*
+---------------------------------------------------------------------------------
+	Ext2 Super Block
+---------------------------------------------------------------------------------
+*/
+struct ext2_super_block
+{
+	__le32	s_inodes_count;
+	__le32	s_blocks_count;
+	__le32	s_r_blocks_count;				/* reserved blocks count			*/
+	__le32	s_free_blocks_count;
+	__le32	s_free_inodes_count;
+	__le32	s_first_data_block;
+	__le32	s_log_block_size;
+	__le32	s_log_frag_size;
+	__le32	s_blocks_per_group;
+	__le32	s_frags_per_group;
+	__le32	s_inodes_per_group;
+	__le32	s_mtime;						/* mount time						*/
+	__le32	s_wtime;						/* write time						*/
+	__le16	s_mnt_count;
+	__le16	s_max_mnt_count;
+	__le16	s_magic;
+	__le16	s_state;						/* file system state				*/
+	__le16	s_errors;						/* behaviour when detecting error	*/
+	__le16	s_minor_rev_level;				/* minor revision level				*/
+	__le32	s_lastcheck;					/* time of last check				*/
+	__le32	s_checkinterval;				/* max. time between checks			*/
+	__le32	s_creator_os;
+	__le16	s_rev_level;					/* revision level					*/
+	__le16	s_def_resuid;					/* default uid for reserved blocks	*/
+	__le16	s_def_resgid;					/* default gid for reserved blocks	*/
+	/* ------------------------------------------------------------------------ */
+	/* Dynamic Revision															*/
+	/* ------------------------------------------------------------------------ */
+	__le32	s_first_ino;					/* first non-reserved inode			*/
+	__le16	s_inode_size;					/* size of inode structure			*/
+	__le16	s_block_group_nr;				/* block group # of this superblock	*/
+	__le32	s_feature_compat;				/* compatible feature set			*/
+	__le32	s_feature_incompat;				/* incompatible feature set			*/
+	__le32	s_feature_ro_compat;			/* readonly-compatible feature set	*/
+	__u8	s_uuid[ 16 ];					/* 128-bit uuid for volume			*/
+	char	s_volume_name[ 16 ];			/* volume name						*/
+	char	s_last_mounted[ 64 ];			/* directory where last mounted		*/
+	__le32	s_algorithm_usage_bitmap;		/* For compression					*/
+	/* ------------------------------------------------------------------------ */
+	/* Peformance Hints															*/
+	/* ------------------------------------------------------------------------ */
+	__u8	s_prealloc_blocks;				/* # of blocks to try to preallocate*/
+	__u8	s_prealloc_dir_blocks;			/* # of preaalocat for dirs			*/
+	__u16	s_padding1;
+	/* ------------------------------------------------------------------------ */
+	/* Journaling Support														*/
+	/* ------------------------------------------------------------------------ */
+	__u8	s_journal_uuid[ 16 ];			/* uuid of journal superblock		*/
+	__u32	s_journal_inum;					/* inode number of journal file		*/
+	__u32	s_journal_dev;					/* device number of journal file	*/
+	__u32	s_last_orphan;					/* start of list of inodes to delete*/
+	/* ------------------------------------------------------------------------ */
+	/* Directory Indexing Support												*/
+	/* ------------------------------------------------------------------------ */
+	__u32	s_hash_seed[ 4 ];				/* HTREE hash seed					*/
+	__u8	s_def_hash_version;				/* default hash version				*/
+	__u8	s_reserved_char_pad;
+	__u16	s_reserved_word_pad;
+	/* ------------------------------------------------------------------------ */
+	/* Other options															*/
+	/* ------------------------------------------------------------------------ */
+	__le32	s_default_mount_opts;
+	__le32	s_first_meta_bg;				/* first metablock block group		*/
+	__u32	s_reserved[ 190 ];				/* padding to the end				*/
+
+};
+
+/*
+---------------------------------------------------------------------------------
+	Me2fs(Ext2) Super Block Information
+---------------------------------------------------------------------------------
+*/
+struct me2fs_sb_info
+{
+	struct ext2_super_block		*s_esb;
+	struct buffer_head			*s_sbh;
+	
+};
+/*
+==================================================================================
+
+	Management
+
+==================================================================================
+*/
+/*
+_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+	Function	:ME2FS_SB
+	Input		:struct super_block *sb
+				 < vfs super block >
+	Output		:void
+	Return		:struct me2fs_inode_info *
+				 < me2fs inode information
+
+	Description	:get me2fs_sb_info from vfs super block
+_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+*/
+static inline struct me2fs_sb_info *ME2FS_SB( struct super_block *sb )
+{
+	return( ( struct me2fs_sb_info* )sb->s_fs_info );
+}
+
+/*
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+	< Open Functions >
+
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+*/
+#endif	// __ME2FS_H__
