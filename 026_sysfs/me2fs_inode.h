06e06f2060448fb3944ab0ea06a2fa6d4beab2a3
Ninals-GitHub <mail@softwaretechnique.jp>
2016-03-28 20:36:11 +0900

    Step.26 Kernel object and sysfs

---
+++ 026_sysfs/me2fs_inode.h
@@ -0,0 +1,218 @@
+/*********************************************************************************
+	File			: me2fs_inode.h
+	Description		: Definition for inode of my extension2 file system
+
+*********************************************************************************/
+#ifndef	__ME2FS_INODE_H__
+#define	__ME2FS_INODE_H__
+
+
+/*
+==================================================================================
+
+	Prototype Statement
+
+==================================================================================
+*/
+
+/*
+==================================================================================
+
+	DEFINES
+
+==================================================================================
+*/
+
+/*
+==================================================================================
+
+	Management
+
+==================================================================================
+*/
+/*
+---------------------------------------------------------------------------------
+	Address Space Operations
+---------------------------------------------------------------------------------
+*/
+extern const struct address_space_operations me2fs_aops;
+
+/*
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+	< Open Functions >
+
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+*/
+/*
+_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+	Function	:me2fsGetVfsInode
+	Input		:struct super_block *sb
+				 < vfs super block >
+				 unsigned int ino
+				 < inode number for allocating new inode >
+	Output		:void
+	Return		:struct inode *inode
+				 < vfs inode >
+
+	Description	:allocate me2fs inode info, get locked vfs inode, read ext2 inode
+				 from a disk and fill them
+_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+*/
+struct inode *me2fsGetVfsInode( struct super_block *sb, unsigned int ino );
+
+/*
+_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+	Function	:me2fsSetVfsInodeFlags
+	Input		:struct inode *inode
+				 < target vfs inode >
+	Output		:struct inode *inode
+				 < written to inode->i_flags >
+	Return		:void
+
+	Description	:write ext2 inode flags to vfs inode flags
+_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+*/
+void me2fsSetVfsInodeFlags( struct inode *inode );
+
+/*
+_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+	Function	:me2fsSetMe2fsInodeFlags
+	Input		:struct me2fs_inode_info *mei
+				 < me2fs inode information >
+	Output		:struct me2fs_inode_info *mei
+				 < written to i_flags >
+	Return		:void
+
+	Description	:write vfs inode flags to ext2 inode flags
+_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+*/
+void me2fsSetMe2fsInodeFlags( struct me2fs_inode_info *mei );
+
+/*
+----------------------------------------------------------------------------------
+
+	Superblock Operations
+
+----------------------------------------------------------------------------------
+*/
+/*
+_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+	Function	:me2fsWriteInode
+	Input		:struct inode *inode
+				 < vfs inode >
+				 struct writeback_control *wbc
+				 < write back information >
+	Output		:void
+	Return		:int
+				 < result >
+
+	Description	:commit writing an inode
+_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+*/
+int me2fsWriteInode( struct inode *inode, struct writeback_control *wbc );
+
+/*
+----------------------------------------------------------------------------------
+	Helper Functions
+----------------------------------------------------------------------------------
+*/
+/*
+_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+	Function	:dbgPrintVfsInode
+	Input		:struct inode *inode
+				 < vfs inode >
+	Output		:void
+	Return		:void
+
+	Description	:print vfs inode debug information
+_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+*/
+void dbgPrintVfsInode( struct inode *inode );
+
+/*
+_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+	Function	:dbgPrintMe2fsInodeInfo
+	Input		:struct me2fs_inode_info *mei
+				 < me2fs ext2 inode information >
+	Output		:void
+	Return		:void
+
+	Description	:print me2fs inode information
+_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+*/
+void dbgPrintMe2fsInodeInfo( struct me2fs_inode_info *mei );
+
+/*
+_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+	Function	:dbgPrintExt2InodeInfo
+	Input		:struct ext2_inode *ei
+				 < ext2 inode >
+	Output		:void
+	Return		:void
+
+	Description	:print ext2 inode debug information
+_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+*/
+void dbgPrintExt2InodeInfo( struct ext2_inode *ei );
+
+/*
+_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+	Function	:me2fsGetBlock
+	Input		:struct inode *inode
+				 < vfs inode >
+				 sector_t iblock
+				 < block number in file >
+				 struct buffer_head *bh_result
+				 < buffer cache for the block >
+				 int create
+				 < 0 : plain lookup, 1 : creation >
+	Output		:struct buffer_head *bh_result
+				 < buffer cache for the block >
+	Return		:int
+				 < result >
+
+	Description	:get block in file or allocate block for file
+_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+*/
+int me2fsGetBlock( struct inode *inode,
+				   sector_t iblock,
+				   struct buffer_head *bh_result,
+				   int create );
+
+/*
+----------------------------------------------------------------------------------
+
+	Superblock Operations
+
+----------------------------------------------------------------------------------
+*/
+/*
+_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+	Function	:me2fsWriteInode
+	Input		:struct inode *inode
+				 < vfs inode >
+				 struct writeback_control *wbc
+				 < write back information >
+	Output		:void
+	Return		:int
+				 < result >
+
+	Description	:commit writing an inode
+_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+*/
+int me2fsWriteInode( struct inode *inode, struct writeback_control *wbc );
+
+/*
+_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+	Function	:me2fsEvictInode
+	Input		:struct inode *inode
+	Output		:void
+	Return		:void
+
+	Description	:evict inode
+_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+*/
+void me2fsEvictInode( struct inode *inode );
+
+#endif	// __ME2FS_INODE_H
